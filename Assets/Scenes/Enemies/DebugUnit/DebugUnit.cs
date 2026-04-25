03aec51b32145fe70fa3736ece25632940f13d0c
R-Miedzinski <radek.miedzinski@gmail.com>
2026-04-25 22:55:52 +0200

    removed old item adn effect implementation. Start of use item flow

---
+++ Assets/Scenes/Enemies/DebugUnit/DebugUnit.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class DebugUnit : Unit
+{
+    private void Start()
+    {
+        CurrentHealth = maxHealth;
+    }
+
+    public override void TakeDamage(float amount)
+    {
+        CurrentHealth -= amount;
+
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public override void Heal(float amount)
+    {
+        CurrentHealth += amount;
+    }
+
+    public override void Die()
+    {
+        Debug.Log($"{UnitName} has died.");
+    }
+}
